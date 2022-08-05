85cd0afe9827cecbab5d5be83e137225ea9614cf
francysebas <francyn@unicauca.edu.co>
2022-08-05 16:21:06 -0500

    subir clases vue

---
+++ 01.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'afiliados',
+        afiliados: [
+            {nombre:'diego', apellido:'Gomez', estado: false, edad:30},
+            {nombre:'Francy', apellido:'Narvaez', estado: false, edad:30},
+            {nombre:'Sebastián', apellido:'Meneses', estado: false, edad:25},
+            {nombre:'Laura', apellido:'Ordoñez', estado: true, edad:30},
+            {nombre:'Carlos', apellido:'Anacona', estado: false, edad:30}
+        ]
+    }
+})
